d948c68e7a7868a5dd53f03307ac145cbb8a9364
= <=>
2021-07-15 22:59:05 +0800

    feat: update debounce

--- polyfill/debounce.js
+++ polyfill/debounce.js
@@ -1,23 +1,18 @@
-// 在一个事件发生一定时间之后，才执行特定动作。
-// 先注册，延迟执行。如果触发重新开始。
-// 没有执行，立即执行。延迟重制。如果触发，重新开始计算重制
 function debounce(func, wait, immediate) {
   var timeout;
   return function() {
-    var context = this, args = arguments;
-    
-    if (timeout) clearTimeout(timeout);
+    var cxt = this, args = arguments;
+    clearTimeout(timeout);
     if (immediate) {
-      var callNow = !timeout;
+      let calledNow = !timeout;
       timeout = setTimeout(() => {
         timeout = null;
       }, wait);
-      if (callNow) func.apply(context, args);
+      if (calledNow) func.apply(cxt, args);
     } else {
       timeout = setTimeout(() => {
-        func.apply(context, args);
+        func.apply(cxt, args);
       }, wait);
     }
   }
 }
-
