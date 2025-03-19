098d37c319039c1d2b98acadbbd9d1fa3e5d6a49
luohe <crazy_zhiqiang@sina.com>
2025-03-19 23:25:32 +0800

    leetcode

--- leetcode/88.合并两个有序数组.js
+++ leetcode/88.合并两个有序数组.js
@@ -43,5 +43,44 @@ var merge = function(nums1, m, nums2, n) {
     nums1[i] = sort1[i]
    }
 };
+
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+  if (n == 0) return;
+  let index1 = m - 1, index2 = n - 1;
+  let p = m + n - 1;
+  
+  while(p >= 0 && index2 >= 0) {
+    if (index1 < 0) {
+      nums1[p] = nums2[index2];
+      index2--;
+      p--;
+      continue;
+    }
+
+    if (index2 < 0) {
+      nums1[p] = nums1[index1]
+      index1--;
+      p--;
+    }
+
+    if (nums1[index1] <= nums2[index2]) {
+      nums1[p] = nums2[index2];
+      index2--;
+      p--;
+      continue;
+    } else {
+      nums1[p] = nums1[index1]
+      index1--;
+      p--;
+    }
+  }
+};
 // @lc code=end
 
