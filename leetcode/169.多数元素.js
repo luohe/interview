087b1c8b6a664b68e77b84a98f4170de5a83ea97
luohe <crazy_zhiqiang@sina.com>
2025-03-20 00:20:37 +0800

    feat: leetcode

---
+++ leetcode/169.多数元素.js
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=169 lang=javascript
+ *
+ * [169] 多数元素
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+// 哈希表统计法： 遍历数组 nums ，用 HashMap 统计各数字的数量，即可找出 众数 。此方法时间和空间复杂度均为 O(N) 。
+// 数组排序法： 将数组 nums 排序，数组中点的元素 一定为众数。
+// 摩尔投票法： 核心理念为 票数正负抵消 。此方法时间和空间复杂度分别为 O(N) 和 O(1) ，为本题的最佳解法。
+var majorityElement = function(nums) {
+    
+};
+// @lc code=end
+
---
+++ leetcode/LCR 146.螺旋遍历二维数组.js
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=LCR 146 lang=javascript
+ * @lcpr version=30204
+ *
+ * [LCR 146] 螺旋遍历二维数组
+ */
+
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+/**
+ * @param {number[][]} array
+ * @return {number[]}
+ */
+var spiralArray = function(array) {
+  if (array.length <= 0) return [];
+  let m = array.length, n = array[0].length;
+  if (n <= 0) return [];
+
+  
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [[1,2,3],[8,9,4],[7,6,5]]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [[1,2,3,4],[12,13,14,5],[11,16,15,6],[10,9,8,7]]\n
+// @lcpr case=end
+
+ */
+
